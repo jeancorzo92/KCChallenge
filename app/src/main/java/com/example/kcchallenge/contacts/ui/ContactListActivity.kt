f224b07153b679b02e442fda0bfff82e15b29939
Jean <jean.corzo92@gmail.com>
2020-07-22 12:15:16 -0300

    Project creation

---
+++ app/src/main/java/com/example/kcchallenge/contacts/ui/ContactListActivity.kt
@@ -0,0 +1,14 @@
+package com.example.kcchallenge.contacts.ui
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.kcchallenge.R
+
+class ContactListActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_contact_list)
+    }
+
+}
